fed9f47afbfcc37d84a15ed01e6afed91671ccf0
Yilotana1 <tolik.zhilko@gmail.com>
2022-02-23 14:30:09 +0200

    Added some comments to code

--- src/util/MathUtil.java
+++ src/util/MathUtil.java
@@ -3,6 +3,9 @@ package util;
 public class MathUtil {
 
 
+    // Округлення числа до певної цифри після коми
+    // double value - число для округлення
+    // int n - кількість цифр після коми
     public static double round(double value, int n){
         return (double)(Math.round(value * Math.pow(10, n))) / Math.pow(10, n);
     }
